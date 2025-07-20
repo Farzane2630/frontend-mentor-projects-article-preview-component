80c6731fb4f29b982f58f3e5e0a69440e1de2ab2
farzane <farzanekazemi8517@gmail.com>
2025-07-20 19:01:49 +0330

    del: removing tooltip

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const tooltip = document.querySelector(".tooltip");
 for (let btn of shareBtns) {
   
   btn.addEventListener("click", () => {
-    const isDesktop = window.innerWidth >= 640;
-    if(isDesktop){
-      tooltip.classList.toggle("visually-hidden");
-    }else {
+    const isMobile = window.innerWidth <= 640;
+    if(isMobile){
+      // tooltip.classList.toggle("visually-hidden");
       socialLinks.classList.toggle("visually-hidden");
       authorSection.classList.toggle("visually-hidden");
+    }else {
+      socialLinks.classList.toggle("visually-hidden");
     }
   //  best practice for screen readers
     btn.setAttribute("aria-expanded",btn.getAttribute("aria-expanded") === "true" ? "false" : "true");
